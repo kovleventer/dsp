ab6e9a2b87967cfaaf84a805c263228e0ad396bc
Kovleventer <kovleventer@gmail.com>
2021-10-12 13:57:50 +0200

    mSDFT finished, added test run result images

--- sdft.py
+++ sdft.py
@@ -3,14 +3,11 @@ import scipy.signal
 import matplotlib.pyplot as plt
 import collections
 
-t = np.linspace(0, 10, 2000)
-x = np.concatenate([
-    np.sin(2*np.pi*t),
-    scipy.signal.square(2 * np.pi * t),
-    scipy.signal.sawtooth(2 * np.pi * t)
-])
 
-#x = np.arange(10)+1
+np.random.seed(0)
+x = np.random.randn(100000)
+#x = np.tile(np.random.randn(1000), 100)
+
 
 class SDFT():
     def __init__(self, size=10):
@@ -30,9 +27,9 @@ class SDFT():
         else:
             new = np.exp(1j * 2 * np.pi * np.arange(self.size) / self.size) * (self.last_result + x - lastx)
 
-        self.last_result = new
+        self.last_result = new.astype(np.csingle)
 
-        return new
+        return new.astype(np.csingle)
 
 class mSDFT():
     def __init__(self, size=10):
@@ -48,17 +45,18 @@ class mSDFT():
         lastx = self.buffer.popleft()
         self.buffer.append(x)
         if self.cnt == self.size:
-            new = np.fft.fft(np.array(self.buffer))
+            new = np.fft.fft(np.array(self.buffer, dtype=np.csingle)) / np.exp(-1j * 2 * np.pi * np.arange(self.size) * (self.cnt % self.size) / self.size)
         else:
-            new = self.last_result + np.exp(-1j * 2 * np.pi * np.arange(self.size) * (self.cnt-1) / self.size) * (x - lastx)
+            new = self.last_result + np.exp(-1j * 2 * np.pi * np.arange(self.size) * ((self.cnt-1) % self.size) / self.size) * (x - lastx)
 
-        self.last_result = new
+        self.last_result = new.astype(np.csingle)
 
-        return new * np.exp(1j * 2 * np.pi * np.arange(self.size) * (self.cnt-1) / self.size)
+        return (new * np.exp(1j * 2 * np.pi * np.arange(self.size) * (self.cnt % self.size) / self.size)).astype(np.csingle)
 
 class MOVDFT():
-    def __init__(self, size=10):
+    def __init__(self, size=10, type=np.csingle):
         self.buffer = collections.deque(maxlen=size)
+        self.type=type
         self.size = size
         for i in range(size):
             self.buffer.append(0)
@@ -67,48 +65,50 @@ class MOVDFT():
         self.buffer.popleft()
         self.buffer.append(x)
 
-        return np.fft.fft(np.array(self.buffer))
+        return np.fft.fft(np.array(self.buffer).astype(self.type)).astype(self.type)
 
-windowsize = 5
-plotted_bin = 3
+windowsize = 25
 
 sdft = SDFT(windowsize)
 msdft = mSDFT(windowsize)
-movdft = MOVDFT(windowsize)
-res_sdft = np.zeros_like(x, dtype=np.csingle)
-res_msdft = np.zeros_like(x, dtype=np.csingle)
-res_movdft = np.zeros_like(x, dtype=np.csingle)
+movdft_f32 = MOVDFT(windowsize)
+movdft_f64 = MOVDFT(windowsize, type=np.cdouble)
+res_sdft = np.zeros((len(x), windowsize), dtype=np.csingle)
+res_msdft = np.zeros((len(x), windowsize), dtype=np.csingle)
+res_movdft_f32 = np.zeros((len(x), windowsize), dtype=np.csingle)
+res_movdft_f64 = np.zeros((len(x), windowsize), dtype=np.cdouble)
 
 for i, point in enumerate(x):
-    res_sdft[i] = sdft.process_point(point)[plotted_bin]
-    res_msdft[i] = msdft.process_point(point)[plotted_bin]
-    res_movdft[i] = movdft.process_point(point)[plotted_bin]
+    res_sdft[i] = sdft.process_point(point)
+    res_msdft[i] = msdft.process_point(point)
+    res_movdft_f32[i] = movdft_f32.process_point(point)
+    res_movdft_f64[i] = movdft_f64.process_point(point)
 
-dif_sdft_movdft = np.abs(res_sdft - res_movdft)
-dif_sdft_msdft = np.abs(res_sdft - res_msdft)
-dif_msdft_movdft = np.abs(res_msdft - res_movdft)
+difasd = res_sdft - res_movdft_f64
 
-dif_alter_sdft_movdft = np.abs(np.abs(res_sdft) - np.abs(res_movdft))
-dif_alter_sdft_msdft = np.abs(np.abs(res_sdft) - np.abs(res_msdft))
-dif_alter_msdft_movdft = np.abs(np.abs(res_msdft) - np.abs(res_movdft))
+dif_sdft = np.mean(np.abs(res_movdft_f64 - res_sdft), axis=1)
+dif_msdft = np.mean(np.abs(res_movdft_f64 - res_msdft), axis=1)
+dif_movdft = np.mean(np.abs(res_movdft_f64 - res_movdft_f32), axis=1)
+
 
 def plot_x():
     plt.plot(x, color="y")
 
-def plot_dfts():
-    plt.plot(np.abs(res_sdft), color="r", alpha=0.5)
-    plt.plot(np.abs(res_msdft), color="g", alpha=0.5)
-    plt.plot(np.abs(res_movdft), color="b", alpha=0.5)
+
+def plot_dfts(bin=3):
+    #plt.plot(np.abs(res_sdft[:,bin].real), color="r", alpha=0.5)
+    plt.plot(np.abs(res_msdft[:,bin].real), color="g", alpha=0.5)
+    #plt.plot(np.abs(res_movdft_f32[:,bin].real), color="b", alpha=0.5)
+    plt.plot(np.abs(res_movdft_f64[:,bin].real), color="y", alpha=0.5)
+
 
 def plot_diffs():
-    plt.plot(dif_msdft_movdft, color="r", alpha=0.5)
-    plt.plot(dif_sdft_movdft, color="g", alpha=0.5)
-    plt.plot(dif_sdft_msdft, color="b", alpha=0.5)
+    plt.plot(dif_sdft, color="r", alpha=0.5, label="SDFT error")
+    plt.plot(dif_msdft, color="g", alpha=0.5, label="mSDFT error")
+    plt.plot(dif_movdft, color="b", alpha=0.5, label="Moving DFT error")
+    plt.legend()
 
-def plot_alter_diffs():
-    plt.plot(dif_alter_msdft_movdft, color="r", alpha=0.5)
-    plt.plot(dif_alter_sdft_movdft, color="g", alpha=0.5)
-    plt.plot(dif_alter_sdft_msdft, color="b", alpha=0.5)
 
-plot_alter_diffs()
-plt.show()
+plot_diffs()
+plt.savefig("NOREPEAT_0.png")
+#plt.show()
