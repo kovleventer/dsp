970937e95d44e698b80332283e6ffb36b9e4a20e
Kovleventer <kovleventer@gmail.com>
2021-12-07 00:28:05 +0100

    Added positive frequency (real sum) osdft

--- sdft.py
+++ sdft.py
@@ -6,8 +6,8 @@ import collections
 from forcedarray import csZeros, csArray, csExp, csSum, csDot
 
 np.random.seed(0)
-#x = np.random.randn(100000).astype(np.single)
-x = np.tile(np.random.randn(1000), 100)
+x = np.random.randn(100000).astype(np.single)
+#x = np.tile(np.random.randn(1000), 100)
 
 
 def get_range(k, mul):
@@ -98,6 +98,38 @@ class oSDFT():
 
         return fix_array((self.internal * c)).array
 
+class positive_oSDFT():
+    def __init__(self, size=10):
+        self.buffer = collections.deque(maxlen=size)
+        self.size = size
+        self.internal = csZeros(size)
+        self.cnt = 0
+        for i in range(size):
+            self.buffer.append(0.)
+        self.wk = get_range(csArray(np.arange(self.size)), 1 / self.size).conj()
+
+    def process_point(self, x):
+        self.cnt += 1
+        self.buffer.popleft()
+        self.buffer.append(x)
+
+        idx = np.mod((self.cnt - 1) * np.arange(self.size), self.size)
+        c = csArray(self.wk.array[idx])
+        g = c.conj()
+
+        mul = c * self.internal
+        y = np.sum(mul.array.real[1:self.size//2])
+        y += mul.array[0].real/2
+        y += mul.array[self.size//2].real/2
+        new = self.internal + (g * (x - y*2)) * (1 / self.size)
+        new = fix_array(new)
+        self.internal = new
+
+        idx = np.mod((self.cnt) * np.arange(self.size), self.size)
+        c = csArray(self.wk.array[idx])
+
+        return fix_array((self.internal * c)).array
+
 class resoSDFT():
     def __init__(self, size=10):
         self.buffer = collections.deque(maxlen=size)
@@ -135,17 +167,19 @@ class MOVDFT():
 
         return fix_array2(np.fft.fft(np.array(self.buffer).astype(self.type)).astype(self.type) / self.size)
 
-windowsize = 4
+windowsize = 64
 
 sdft = SDFT(windowsize)
 msdft = mSDFT(windowsize)
 osdft = oSDFT(windowsize)
+pososdft = positive_oSDFT(windowsize)
 resosdft = resoSDFT(windowsize)
 movdft_f32 = MOVDFT(windowsize)
 movdft_f64 = MOVDFT(windowsize, type=np.cdouble)
 res_sdft = np.zeros((len(x), windowsize), dtype=np.csingle)
 res_msdft = np.zeros((len(x), windowsize), dtype=np.csingle)
 res_osdft = np.zeros((len(x), windowsize), dtype=np.csingle)
+res_pososdft = np.zeros((len(x), windowsize), dtype=np.csingle)
 res_resosdft = np.zeros((len(x), windowsize), dtype=np.csingle)
 res_movdft_f32 = np.zeros((len(x), windowsize), dtype=np.csingle)
 res_movdft_f64 = np.zeros((len(x), windowsize), dtype=np.cdouble)
@@ -154,6 +188,7 @@ for i, point in enumerate(x):
     res_sdft[i] = sdft.process_point(point)
     res_msdft[i] = msdft.process_point(point)
     res_osdft[i] = osdft.process_point(point)
+    res_pososdft[i] = pososdft.process_point(point)
     res_resosdft[i] = resosdft.process_point(point)
     res_movdft_f32[i] = movdft_f32.process_point(point)
     res_movdft_f64[i] = movdft_f64.process_point(point)
@@ -163,6 +198,7 @@ for i, point in enumerate(x):
 dif_sdft = np.mean(np.abs(res_movdft_f64 - res_sdft), axis=1)
 dif_msdft = np.mean(np.abs(res_movdft_f64 - res_msdft), axis=1)
 dif_osdft = np.mean(np.abs(res_movdft_f64 - res_osdft), axis=1)
+dif_pososdft = np.mean(np.abs(res_movdft_f64 - res_pososdft), axis=1)
 dif_resosdft = np.mean(np.abs(res_movdft_f64 - res_resosdft), axis=1)
 dif_movdft = np.mean(np.abs(res_movdft_f64 - res_movdft_f32), axis=1)
 
@@ -187,7 +223,8 @@ def plot_diffs():
     #plt.plot(dif_sdft, color="r", alpha=0.5, label="SDFT error")
     #plt.plot(dif_msdft, color="g", alpha=0.5, label="mSDFT error")
     plt.plot(dif_osdft, color="y", alpha=0.5, label="oSDFT error")
-    plt.plot(dif_resosdft, color="orange", alpha=0.5, label="Resonator oSDFT error")
+    plt.plot(dif_pososdft, color="purple", alpha=0.5, label="real oSDFT error")
+    #plt.plot(dif_resosdft, color="orange", alpha=0.5, label="Resonator oSDFT error")
     plt.plot(dif_movdft, color="b", alpha=0.5, label="Moving DFT error")
     plt.legend()
 
@@ -200,5 +237,5 @@ def plot_selfdiffs():
     plt.legend()
 
 plot_diffs()
-plt.savefig("osdft_4_repeat.png")
+#plt.savefig("osdft_4_repeat.png")
 plt.show()
