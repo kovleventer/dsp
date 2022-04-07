763dffd5fc3a6f30c1a7bc4d0c9220d6ed055917
Kovleventer <kovleventer@gmail.com>
2022-04-07 21:47:32 +0200

    Added dither

--- iir/iir.py
+++ iir/iir.py
@@ -23,6 +23,11 @@ snr_refs = []
 
 for n_frac in range(3, 33):
     fxp_ref = Fxp(None, signed=True, n_word=n_frac + 15, n_frac=n_frac)
+
+    def Q(x):
+        return Fxp(x, like=fxp_ref).get_val()
+
+    q = fxp_ref.precision
     x_q = Fxp(x, like=fxp_ref)
     x_qq = x_q.get_val()
     #x_qq = x
@@ -30,28 +35,36 @@ for n_frac in range(3, 33):
     N = 64
     output_ref = np.zeros(len(x))
     output_fxp = np.zeros(len(x))
+    output_fxp_d = np.zeros(len(x))
     state_ref = np.zeros(len(x)+1)
     state_fxp = np.zeros(len(x)+1)
+    state_fxp_d = np.zeros(len(x) + 1)
     a = 0.9
     for i in range(len(x)):
 
         output_ref[i] = state_ref[i] * a + x_qq[i]
         state_ref[i+1] = output_ref[i]
 
-        output_fxp[i] = Fxp(Fxp(state_fxp[i] * a, like=fxp_ref).get_val() + x_qq[i], like=fxp_ref).get_val()
+        output_fxp[i] = Q(Q(state_fxp[i] * a) + x_qq[i])
         state_fxp[i+1] = output_fxp[i]
+
+        d1 = np.random.uniform(-q/2, q/2)
+        d2 = np.random.uniform(-q/2, q/2)
+        output_fxp_d[i] = Q(Q(state_fxp_d[i] * a + d1) + x_qq[i] + d2)
+        state_fxp_d[i + 1] = output_fxp_d[i]
 
     """plt.plot(x_qq, label="x")
     plt.plot(output_ref, label="reference filter output")
     plt.plot(output_fxp, label="fxp filter output")
+    plt.plot(output_fxp_d, label="fxp dither filter output")
     plt.legend()
-    plt.savefig("iir_" + str(n_frac) + ".png")
+    plt.savefig("iir_" + str(n_frac) + "_dither.png")
     plt.show()"""
 
     Ps = np.mean(np.abs(output_ref) ** 2)
     Pn = np.mean(np.abs(output_ref - output_fxp) ** 2)
     Psfxp = np.mean(np.abs(output_fxp) ** 2) - Pn
-    Pnexp = (fxp_ref.precision ** 2) / 6 / (1-a**2)
+    Pnexp = (q ** 2) / 6 / (1-a**2)
 
     snr = 10 * np.log10(Psfxp / Pn)
     snr_ref = 10 * np.log10(Ps / Pnexp)
